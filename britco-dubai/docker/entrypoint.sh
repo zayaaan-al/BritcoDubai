9aad3ac8b1c57dab03879cdba23ed0fb4b8eb09d
Al Zayan P <alzayanpoothakkal1156@gmail.com>
2026-06-07 20:40:46 +0530

    Fix session and cache drivers

--- britco-dubai/docker/entrypoint.sh
+++ britco-dubai/docker/entrypoint.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-# Run database migrations
+php artisan config:clear
+php artisan cache:clear
+
 php artisan migrate --force
 
-# Cache Laravel files
 php artisan config:cache
-php artisan route:cache
-php artisan view:cache
 
-# Start PHP-FPM
 php-fpm -D
-
-# Start Nginx in foreground
 nginx -g "daemon off;"
