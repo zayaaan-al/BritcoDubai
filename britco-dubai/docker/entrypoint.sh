bdbfb3fb7a648c369f641a441503d47363de1ceb
Al Zayan P <alzayanpoothakkal1156@gmail.com>
2026-06-07 20:20:06 +0530

    Fix entrypoint encoding

--- britco-dubai/docker/entrypoint.sh
+++ britco-dubai/docker/entrypoint.sh
@@ -1,15 +1,12 @@
-@'
-#!/bin/bash
+#!/bin/sh
 
-# Create .env from environment variables
 cp /var/www/.env.example /var/www/.env
 
-# Inject env vars into .env
 php artisan key:generate --force
 php artisan migrate --force
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
+
 php-fpm -D
 nginx -g "daemon off;"
-'@ | Set-Content docker/entrypoint.sh -NoNewline
